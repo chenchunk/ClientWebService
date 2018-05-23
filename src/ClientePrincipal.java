9e64d68a632072d65a3e3697d5b9d17c824a0841
0138905 <0138905@206v-16.acad.feevale.br>
2018-05-22 21:25:00 -0300

    Criação do projeto

---
+++ src/ClientePrincipal.java
@@ -0,0 +1,20 @@
+import br.feevale.*;
+import sun.print.resources.serviceui;
+/**
+ *
+ * @author 0138905
+ */
+public class ClientePrincipal {
+    
+    public static void main(String[] args){
+        WService_Service service = new WService_Service();
+        
+        WService servicoSoma = service.getWServicePort();
+        
+        double valorResposta = servicoSoma.soma(20, 40);
+        
+        System.out.println("Resposta = " + valorResposta);
+    
+    }
+    
+}
